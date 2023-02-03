97b5acd2c1e72979b90555db01f64f3355d55a2f
marcosdio <marcosdio@gmail.com>
2023-02-02 22:06:17 -0300

    Initial commit

--- acesso.R
+++ acesso.R
@@ -1,2 +1,3 @@
 library(usethis)
 use_git()
+write_csv(cnpj_list, "banco_dados_cnpj")
--- web_scraping_with_RSelenium.R
+++ web_scraping_with_RSelenium.R
@@ -4,10 +4,13 @@
 library(tidyverse)
 library(RSelenium)
 library(netstat)
+library(readxl)
+library(writexl)
 
 
 rs_driver_object <- rsDriver(browser = "firefox",
                              chromever = NULL,
+                             verbose=FALSE,
                              port = free_port())
 
 remDr <-  rs_driver_object$client
@@ -16,16 +19,18 @@ remDr <-  rs_driver_object$client
 site_sancoes <- "https://www.bec.sp.gov.br/sancoes_ui/aspx/consultaadministrativafornecedor.aspx"
 
 
-cnpj_list <- c(45039237000114,27865757000102,12546935000157,07065868000119,60701190000104,60746948000112,03788306000142)
+cnpj_list <- read_excel("banco_cnpj.xlsx")
 
 
 funcao_sancoes <- function(cnpj_empresas){
   remDr$navigate(site_sancoes)
-  remDr$findElement(using= "id", "txtCNPJCPF")$sendKeysToElement(c(32232246884, key = "enter"))
+  remDr$findElement(using= "id", "txtCNPJCPF")$sendKeysToElement(c(cnpj_empresas, key = "enter"))
   Sys.sleep(2)
-  if(substr(remDr$findElement(using='css selector', 'table.styled:nth-child(3) > tbody:nth-child(1)')$getElementText(), 74, 102)=="Não foram encontradas sanções"){
+  css_obj <- 'table.styled:nth-child(3) > tbody:nth-child(1)'
+  if(substr(remDr$findElement(using='css selector', css_obj)$getElementText(), 74, 102)=="Não foram encontradas sanções"){
     print("ok")
   }else {print("pendencia")}
   }
 
-results <- ?sapply(cnpj_list, FUN = funcao_sancoes)
+write_xlsx(cbind(cnpj_list,sapply(cnpj_list$cnpj, FUN = funcao_sancoes)), "resultado e-sancoes.xlsx")
+
